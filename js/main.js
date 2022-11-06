2519a3a137b6e757c541f2043a66e9be22199684
dimstud <dimichageev@gmail.com>
2022-11-06 12:43:23 +0300

    Выполнил 1 задание 2 раздела

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let x1 = 2,
+  x2 = 10,
+  y1 = 3,
+  y2 = 5;
+  
+  //let dot1 = Math.abs(x1 - x2), dot2 = Math.abs(y1 - y2);
+  
+  //let totall = Math.sqrt(Math.pow(dot1, 2) + Math.pow(dot2, 2));
+  let rectangleArrea = Math.abs(x1 - x2) * Math.abs(y1 - y2);
+  console.log(rectangleArrea);
+
+//S = ((x2 - x1)2 + (y2 - y1)2) / 2
+
